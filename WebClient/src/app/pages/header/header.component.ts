5ecc9dc89a9a918f478cb04273dba8b1bfc2c68c
huy2103 <lhuymel2@gmail.com>
2024-06-26 23:54:04 +0700

    feat: link login to backend

--- WebClient/src/app/pages/header/header.component.ts
+++ WebClient/src/app/pages/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Users } from '../../interfaces/users';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +11,41 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userDetail: Users | null = null;
+  fetchUserDetail: boolean = false;
+  
+  authService = inject(AuthService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
 
+  constructor() {}
+
+  ngOnInit(): void {
+    try {
+      this.authService.getUserInfo().subscribe(data => {
+        this.userDetail = data;
+        this.fetchUserDetail = true;
+        this.updateUserName(data.userName); // Call to update the username on successful fetch
+      }, error => {
+        console.error('Failed to fetch user details', error);
+        this.fetchUserDetail = false;
+      });
+    } catch (error) {
+      console.error('Error retrieving token', error);
+      this.fetchUserDetail = false;
+    }
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  updateUserName(username: string): void {
+    // Assuming you have an element with the id 'userName' to display the username
+    const userNameElement = document.getElementById('userName');
+    if (userNameElement) {
+      userNameElement.textContent = username;
+    }
+  }
 }
--- WebClient/src/app/services/auth.service.ts
+++ WebClient/src/app/services/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { RegisterRequest } from '../interfaces/register-request';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AuthResponse } from '../interfaces/auth-response';
 import { environment } from '../../environments/environment.development';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginRequest } from '../interfaces/login-request';
+import { jwtDecode } from 'jwt-decode';
+import { Users } from '../interfaces/users';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +15,56 @@ export class AuthService {
   apiUrl: string = environment.apiUrl;
   private userKey = 'user';
 
-  
+
   constructor(private http: HttpClient) { }
 
-  
+
   register(data: RegisterRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.apiUrl}auth/register`, data);
   }
-
-
-}
+  login(data: LoginRequest): Observable<AuthResponse> {
+    return this.http
+      .post<AuthResponse>(`${this.apiUrl}auth/login`, data)
+      .pipe(
+        map((response) => {
+          if (response) {
+            localStorage.setItem(this.userKey, JSON.stringify(response));
+          }
+          return response;
+        })
+      );
+    }
+    getUserInfo(): Observable<Users> {
+      const token = this.getToken();
+      if (!token) {
+        throw new Error('No token found');
+      }
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+      return this.http.get<Users>(`${this.apiUrl}auth/me`, { headers });
+    }
+  
+    isLoggedIn = (): boolean => {
+      const token = this.getToken();
+      if (!token) return false;
+      return true;
+    };
+  
+    private isTokenExpired() {
+      const token = this.getToken();
+      if (!token) return true;
+      const decoded = jwtDecode(token);
+      const isTokenExpired = Date.now() >= decoded['exp']! * 1000;
+      // if (isTokenExpired) this.logout();
+      return true;
+    }
+  
+    getToken = (): string | null => {
+      const user = localStorage.getItem(this.userKey);
+      if (!user) return null;
+      const userDetail: AuthResponse = JSON.parse(user);
+      return userDetail.token;
+    };
+  
+  }
